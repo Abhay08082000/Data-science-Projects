a144b9a0bdfdc8183bdccbc31c281c207ecb49cf
Abhay08082000 <bishtabhay8@gmail.com>
2023-10-22 18:26:38 +0530

    Add your commit

--- Family Guy Image Classification/streamlit.py
+++ Family Guy Image Classification/streamlit.py
@@ -5,12 +5,13 @@ from img2vec_pytorch import Img2Vec
 import torchvision.transforms as transforms
 img2vec = Img2Vec()
 
+
 st.set_option('deprecation.showfileUploaderEncoding',False)
 @st.cache(allow_output_mutation=True)
 def load_model(path):
     model=pickle.load(open(path,"rb"))
     return model
-SVC=load_model(r"C:\\Users\\bisht\\OneDrive\\Desktop\\Family guy\\Family Guy Image Classification\\sv.pkl")
+SVC=load_model(r"C:\\Users\\bisht\\OneDrive\\Desktop\\Family guy\\Family Guy Image Classification\\lr.pkl")
 st.write("""
          #FamilyGuy Image Classification
          """
@@ -20,12 +21,12 @@ import cv2 as cv
 from PIL import Image,ImageOps
 transform = transforms.Compose([
         transforms.Resize((224, 224)),  # Resize the image to a specific size
-        transforms.ToTensor(),  # Convert PIL Image to PyTorch Tensor
+        transforms.ToTensor()  # Convert PIL Image to PyTorch Tensor
     ])
 
 def image_and_predict(image_data,model):
 
-    img = transform(image)
+    img = transform(image_data)
     img_pil = transforms.functional.to_pil_image(img)
     img_features = img2vec.get_vec(img_pil)
     prediction=model.predict([img_features])
@@ -33,7 +34,7 @@ def image_and_predict(image_data,model):
 if file is None:
     st.text("Please upload the image file")
 else:
-    image=Image.open(file)
+    image=Image.open(file).convert('RGB')
     st.image(image,use_column_width=True)
     prediction=image_and_predict(image,SVC)
     pred=prediction[0]
